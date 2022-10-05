90931f4caed4006aa09ddf09d88d47824e313c4f
haneul0220 <wwoww00@naver.com>
2022-10-05 14:15:04 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
 def print_hello():
     print("Hello World!!")
 
+def print_KIT():
+	print("Kumoh National
+Insititue of Technology.")
+
 if __name__ == '__main__':
-    print_hello()
+	print_KIT()
+	print_hello()
